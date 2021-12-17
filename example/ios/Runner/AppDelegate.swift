d34dd7f9a954847140622373a8926d896a919c96
Yuri <yuraware@gmail.com>
2021-12-17 23:58:43 +0200

    Add video capture permission request

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import AVFoundation
 import Flutter
 
 @UIApplicationMain
@@ -7,7 +8,27 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+      GeneratedPluginRegistrant.register(with: self)
+      
+      switch AVCaptureDevice.authorizationStatus(for: .video) {
+          case .authorized:
+          // The user has previously granted access to the camera.
+          break
+          case .notDetermined: // The user has not yet been asked for camera access.
+              AVCaptureDevice.requestAccess(for: .video) { granted in
+                  if granted {
+                      //TODO request
+                  }
+              }
+          
+          case .denied: // The user has previously denied access.
+              return
+
+          case .restricted: // The user can't grant access due to restrictions.
+              return
+      }
+
+      
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
