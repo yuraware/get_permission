6bf20a9b9aa9df8d671274c601b8ca1a82481df4
Yuri <yuraware@gmail.com>
2021-12-19 23:59:17 +0200

    Add permission handling

--- ios/Classes/SwiftGetPermissionPlugin.swift
+++ ios/Classes/SwiftGetPermissionPlugin.swift
@@ -2,13 +2,25 @@ import Flutter
 import UIKit
 
 public class SwiftGetPermissionPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "get_permission", binaryMessenger: registrar.messenger())
-    let instance = SwiftGetPermissionPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "get_permission", binaryMessenger: registrar.messenger())
+        let instance = SwiftGetPermissionPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result("iOS " + UIDevice.current.systemVersion)
+    }
+    
+    private func checkPermission(type: PermissionType) {
+        switch (type) {
+        case .camera, .microphone:
+            let handler = AudioVideoHandler()
+            handler.checkStatus(type)
+        case .contacts:
+            fatalError()
+        default:
+            fatalError()
+        }
+    }
 }
