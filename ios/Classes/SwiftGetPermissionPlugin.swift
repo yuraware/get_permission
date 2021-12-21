19f438b95636447742b9626ac10c6aba4a778ebe
Yuri <yuraware@gmail.com>
2021-12-21 23:52:32 +0200

    Add method channel for checking permission, availability

--- ios/Classes/SwiftGetPermissionPlugin.swift
+++ ios/Classes/SwiftGetPermissionPlugin.swift
@@ -7,19 +7,47 @@ public class SwiftGetPermissionPlugin: NSObject, FlutterPlugin {
         let instance = SwiftGetPermissionPlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
-    
+
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        result("iOS " + UIDevice.current.systemVersion)
+        
+        guard let arguments = call.arguments as? ArgumentsParser,
+              let permission = arguments.parseInt(),
+              let type = PermissionType(rawValue: permission) else {
+            fatalError("Integer argument should be passed to the plugin")
+        }
+        
+        
+        switch (call.method) {
+        case "checkPermission":
+            checkPermission(type: type) { status in
+                result(status)
+            }
+        case "checkAvailability":
+            checkAvailability(type: type) { availability in
+                result(availability)
+            }
+        default:
+            fatalError("Not implemented method channel: \(call.method)")
+        }
+    }
+
+    private func checkPermission(type: PermissionType, result: FlutterResult) {
+        result(handler(for: type).checkStatus(type).rawValue)
+    }
+ 
+    private func checkAvailability(type: PermissionType, result: FlutterResult) {
+        let handler = handler(for: type)
+        handler.checkAvailability(type) { availability in
+            result(availability.rawValue)
+        }
     }
     
-    private func checkPermission(type: PermissionType) {
+
+    private func handler(for type: PermissionType) -> HandlerProtocol {
         switch (type) {
         case .camera, .microphone:
-            let handler = AudioVideoHandler()
-            handler.checkStatus(type)
+            return AudioVideoHandler()
         case .contacts:
-            fatalError()
-        default:
             fatalError()
         }
     }
