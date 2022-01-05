201376a5717fc8eab0f1b07eb4a2aaeb05b1818c
Yuri <yuraware@gmail.com>
2022-01-05 21:13:52 +0200

    Add check multiple permission statuses

--- lib/get_permission.dart
+++ lib/get_permission.dart
@@ -36,6 +36,18 @@ class _GetPermission extends PermissionHandler {
           Permissions.values[key], StatusParser.statusFrom(val));
     });
   }
+
+  static Future<Map<Permissions, Status>> checkStatuses(
+      List<Permissions> permissions) async {
+    final permissionIndices = permissions.map((p) => p.index).toList();
+    final statuses =
+        await _channel.invokeMethod('checkPermissions', permissionIndices);
+
+    return Map<int, int>.from(statuses).map((key, val) {
+      return MapEntry<Permissions, Status>(
+          Permissions.values[key], StatusParser.statusFrom(val));
+    });
+  }
 }
 
 extension StatusParser on Status {
@@ -75,5 +87,9 @@ extension GetPermissionsHandler on List<Permissions> {
   ///
   Future<Map<Permissions, Status>> request() async {
     return _GetPermission.requestPermissions(this);
+  }
+
+  Future<Map<Permissions, Status>> checkStatuses() async {
+    return _GetPermission.checkStatuses(this);
   }
 }
