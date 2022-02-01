12617957818eae37e0831b10e0b5aef579205a48
Yuri <yuraware@gmail.com>
2022-02-01 23:52:53 +0200

    Add check of new permissions in example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -96,6 +96,19 @@ class _MyAppState extends State<MyApp> {
           'Request location permission status - $k, status: ${locationStatuses[k]}');
     }
 
+    final photosStatuses = await [
+      Permissions.photos,
+      Permissions.photosWriteOnlyIOS,
+    ].request();
+    for (var k in statuses.keys) {
+      debugPrint(
+          'Request photo permission status - $k, status: ${photosStatuses[k]}');
+    }
+
+    final requestMediaLibraryStatus = Permissions.mediaLibrary.request();
+    debugPrint(
+        'Request media library permission status: $requestMediaLibraryStatus');
+
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
