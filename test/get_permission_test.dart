f4274af63a66c5c008e7e17c846acc969e0bd20d
Yuri <yuraware@gmail.com>
2022-02-08 22:46:34 +0200

    Update tests

--- test/get_permission_test.dart
+++ test/get_permission_test.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_permission/get_permission.dart';
+import 'package:get_permission/permission.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('get_permission');
 
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
+  test('Expect statuses', () {
+    expect(StatusParser.statusFrom(0), Status.denied);
+    expect(StatusParser.statusFrom(1), Status.authorized);
+    expect(StatusParser.statusFrom(2), Status.restrictedIOS);
+    expect(StatusParser.statusFrom(3), Status.limitedIOS);
+    expect(StatusParser.statusFrom(4), Status.permanentlyDeniedAndroid);
+    expect(StatusParser.statusFrom(5), Status.notSupported);
   });
 }
